a814e999fd546ccbecaf4566b7ead5fb308d9edc
giantframe <singingland@gmail.com>
2019-05-18 15:50:46 -0400

    sightline

--- Eye.gd
+++ Eye.gd
@@ -1,13 +1,21 @@
 extends AnimatedSprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var point_array = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	point_array = [position, $SightRay.get_collision_point()]
+	$SightLine.position = Vector2(-position.x, -position.y)
+	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	play()
+	
+	if $SightRay.is_colliding():
+		print(point_array)
+		point_array = [position, $SightRay.get_collision_point()-(position)]
+	
+	$SightLine.set_points(point_array)
+
+	
